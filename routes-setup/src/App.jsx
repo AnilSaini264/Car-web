38018016ffc473e5c61bde4021d543b2f44cdd51
AnilSaini264` <https://github.com/AnilSaini264`>
2026-07-07 17:48:41 +0530

    Saini

---
+++ routes-setup/src/App.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Products from "./pages/Products";
+import Contact from "./pages/Contact";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
